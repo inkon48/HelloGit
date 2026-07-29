ed77aeb453c37ba18705ec7181a69afa52cda453
Inkon <inkon601@gmail.com>
2026-07-29 10:10:11 +0600

    first commit

---
+++ calc.mjs
@@ -0,0 +1,25 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function division(num1, num2) {
+  if (num2 === undefined || num2 === null) {
+    return "Please provide both numbers.";
+  }
+
+  if (num2 === 0) {
+    return "It's not possible to divide by 0!";
+  }
+
+  return num1 / num2;
+}
+
+export { add, subtract, multiply, division };
